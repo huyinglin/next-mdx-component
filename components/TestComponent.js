fd54e88a04b75f57bc9c00e0392155b670244bc5
faz <huyinglinfz@gmail.com>
2022-09-13 23:47:32 +0800

    feat: init project

---
+++ components/TestComponent.js
@@ -0,0 +1,32 @@
+import Highlight, { defaultProps } from "prism-react-renderer";
+import theme from "prism-react-renderer/themes/vsLight";
+
+function TestComponent({ name = 'world', src, demoSource }) {
+  console.log('src: ', src);
+  console.log('----demoSource: ', demoSource);
+  return (
+    <>
+      <div>Hello, {name}!</div>
+      <div style={{ border: '1px solid red', padding: 20 }}>
+        <Highlight {...defaultProps} code={demoSource[0]?.content} theme={theme} language="jsx">
+          {({ className, style, tokens, getLineProps, getTokenProps }) => (
+            <pre className={className} style={style}>
+              {tokens.map((line, i) => (
+                <div {...getLineProps({ line, key: i })}>
+                  {line.map((token, key) => (
+                    <span {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              ))}
+            </pre>
+          )}
+        </Highlight>
+      </div>
+      <style jsx>{``}</style>
+    </>
+  )
+}
+
+export default function test(demoSource) {
+  return (props) => <TestComponent {...props} demoSource={demoSource}/>
+}
